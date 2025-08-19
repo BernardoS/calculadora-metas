6bfb5cc1d37f3633be91eda8574f9a6673ca3fd3
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-08-18 23:32:12 -0300

    feat: implements `ChartPlan` responsiveness

--- src/components/ChartPlan/styles.tsx
+++ src/components/ChartPlan/styles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../../utils/layoutBreakpoints";
 
 export const ChartPlanContainer = styled.div`
     display: flex;
@@ -17,11 +18,9 @@ export const ChartPlanTitle = styled.h2`
     margin-bottom: 24px;
 `
 
-export const ChartContainer = styled.div`
+export const ChartBox = styled.div`
     display: flex;
     justify-content: center;
-    width: '100%';
-    height: '400px';
     margin: 0 64px;
     padding: 16px;
     border-radius: 8px;
@@ -29,4 +28,22 @@ export const ChartContainer = styled.div`
     -webkit-box-shadow: 10px 10px 0px -2px rgba(26,26,26,0.7);
     -moz-box-shadow: 10px 10px 0px -2px rgba(26,26,26,0.7);
     border:2px solid var(--border-default);
+   
+
+    @media ${device.mobile} {
+        margin: 0 8px;
+        overflow-x:scroll; 
+    }
+`;
+
+export const ChartContainer = styled.div`
+    display: flex;
+    height: 400px;
+    width: 100%;
+
+    @media ${device.mobile} {
+        width: 700px;
+        padding-left: 450px;
+    }
+
 `;
